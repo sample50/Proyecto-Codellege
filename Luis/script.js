913030aa145746336b0e448b651ad05f62ce52ae
sample50 <sample50@hotmail.com>
2020-04-20 16:58:55 -0500

    Avances

---
+++ Luis/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var tel = document.getElementById("tel").value;
+  var comm = document.getElementById("comment").value;
+
+  console.log(name);
+
+}
